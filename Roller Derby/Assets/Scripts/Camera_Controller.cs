092b4655103abc4ef46db06b5eadd53aa97020f7
Alexander Luthe <aluthe@sae.au>
2016-08-03 15:23:37 +1000

    Initial Commit of Sprint 1 progress

--- Roller Derby/Assets/Scripts/Camera_Controller.cs
+++ Roller Derby/Assets/Scripts/Camera_Controller.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Camera_Controller : MonoBehaviour {
 
     public GameObject player;
-    public float moveSpeed = 60;
+    public float moveSpeed = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -14,8 +14,10 @@ public class Camera_Controller : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	    Vector3 newPos = new Vector3();
-        newPos.x = Mathf.Lerp(player.transform.position.x, transform.position.x, Time.deltaTime * moveSpeed);
-        newPos.y = Mathf.Lerp(player.transform.position.y + 4.6f, transform.position.y, Time.deltaTime * moveSpeed);
+        //newPos.x = Mathf.Lerp(transform.position.x, player.transform.position.x, Time.deltaTime * moveSpeed);
+        //newPos.y = Mathf.Lerp(transform.position.y, player.transform.position.y + 4.6f, Time.deltaTime * moveSpeed);
+        newPos.x = player.transform.position.x;
+        newPos.y = player.transform.position.y + 4.6f;
         newPos.z = -10;
         transform.position = newPos;
 	}
--- Roller Derby/Assets/Scripts/Player_Controller.cs
+++ Roller Derby/Assets/Scripts/Player_Controller.cs
@@ -5,13 +5,16 @@ using System.Collections;
 public class Player_Controller : MonoBehaviour {
 
     public float moveSpeed;
-    public float maxSpeed = 20;
-    private float accSpeed;
+    float maxSpeed = 10000;
+    float minSpeed = -250;
+    public float accSpeed;
     public Slider Speed;
     public GameObject chassis;
     public GameObject[] wheels;
     public Vector3 chassisPos = new Vector3();
     public float angle;
+    float moveAmount;
+    float prevPos = 0;
 
     /*
     Wheels[0]: Left Back
@@ -20,68 +23,33 @@ public class Player_Controller : MonoBehaviour {
     Wheels[3]: Right Front
     */
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        accSpeed = 20 * Speed.value;
-        if (moveSpeed < maxSpeed)
-        {
-            moveSpeed += accSpeed * Time.deltaTime;
-        }
-        foreach (GameObject wheel in wheels)
-        {
-            wheel.GetComponent<Rigidbody>().AddTorque(transform.right * moveSpeed * Time.deltaTime * 10);
-
-            //Ensure front and back wheels stay the corrent distance apart
-            if(wheel == wheels[0] || wheel == wheels[2])
-            {
-                Vector3 WheelPos = wheel.transform.position;
-                if(wheel == wheels[0])
-                {
-                    WheelPos.x = wheels[1].transform.position.x + 2.74f;
-                    WheelPos.z = wheels[1].transform.position.z;
-                }
-                else if(wheel == wheels[2])
-                {
-                    WheelPos.x = wheels[3].transform.position.x + 2.74f;
-                    WheelPos.z = wheels[3].transform.position.z;
-                }
-                wheel.transform.position = WheelPos;
-            }
-            // Ensure wheels stay in line
-            if(wheel == wheels[0])
-            {
-                wheel.transform.position = new Vector3(wheels[2].transform.position.x, wheel.transform.position.y, wheel.transform.position.z);
-            }
-            else if(wheel == wheels[1])
-            {
-                wheel.transform.position = new Vector3(wheels[3].transform.position.x, wheel.transform.position.y, wheel.transform.position.z);
-            }
-        }
-
-        // Chassis Movement
-        Vector3 WheelOffset = new Vector3((wheels[1].transform.position.x - wheels[2].transform.position.x) / 2, 0.75f, -wheels[0].transform.position.z);
-        chassis.transform.position = wheels[0].transform.position + WheelOffset;
-
-
-        // Chassis Rotation
-        angle = Mathf.Asin((wheels[0].transform.position.y - wheels[1].transform.position.y) / 2.74f) * Mathf.Rad2Deg;
-
-        if(angle < 0)
-        {
-            angle *= 2;
-        }
-        else if(angle > 0)
-        {
-
-        }
-
-        float chassisAngle = chassis.transform.rotation.eulerAngles.x - angle;
-        Quaternion rotation = Quaternion.Euler(270 + angle, 270, 0);
-        chassis.transform.rotation = rotation;
+    // Use this for initialization
+    void Start() {
+        prevPos = transform.position.x;
     }
-    
+
+    // Update is called once per frame
+    void Update() {
+        moveAmount = Mathf.Round(transform.position.x * 100) - Mathf.Round(prevPos * 100);
+        Debug.Log (moveAmount);
+        moveSpeed += Speed.value * 100;
+        moveSpeed = Mathf.Clamp(moveSpeed, minSpeed, maxSpeed);
+
+        foreach (GameObject wheel in wheels) {
+            if ((moveAmount > 0 && Speed.value < 0) || (moveAmount < 0 && Speed.value > 0)) {
+                wheel.GetComponent<WheelCollider>().brakeTorque = maxSpeed * 10;
+                moveSpeed = 0;
+                wheel.GetComponent<WheelCollider>().motorTorque = moveSpeed;
+            }
+            else {
+                wheel.GetComponent<WheelCollider>().motorTorque = moveSpeed;
+                //Debug.Log("MoveSpeed: " + moveSpeed + " BrakeTorque: " + wheel.GetComponent<WheelCollider>().brakeTorque);
+                if (wheel.GetComponent<WheelCollider>().brakeTorque > 0) {
+                    wheel.GetComponent<WheelCollider>().brakeTorque -= maxSpeed;
+                }
+            }
+        }
+        prevPos = transform.position.x;
+    }
+
 }
